741efcaad23b9f711ff8e57908c0f85a44ae3326
Snoo <snoo6586@gmail.com>
2024-06-27 00:46:09 +0300

    shadows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,28 +4,33 @@
     </title>
 </svelte:head>
 
-    <div class=" bg-white h-fit w-fit rounded-3xl grid grid-cols-4  gap-4">
+    <div class=" bg-white h-fit w-fit rounded-3xl grid grid-cols-4  gap-1 p-6 font-semibold text-xl shadow-2xl">
 
-        <button> % </button>
-        <button> √ </button>
-        <button> CE </button>
-        <button> C </button>
+        <div class= "bg-slate-400 rounded-full col-span-4 h-12 flex items-center px-4 mb-2  text-white  text-lg  " >
+            123456
+
+        </div>
+
+        <button class="bg-slate-50"> % </button>
+        <button class="bg-slate-50"> √ </button>
+        <button class="bg-slate-50"> CE </button>
+        <button class="bg-yellow-100  text-gray-600"> C </button>
         <button> 7 </button>
         <button> 8 </button>
         <button> 9 </button>
-        <button> - </button>
+        <button class="bg-violet-100  text-gray-600 "> - </button>
         <button> 4 </button>
         <button> 5 </button>
         <button> 6 </button>
-        <button> ÷ </button>
+        <button class="bg-rose-100  text-gray-600"> ÷ </button>
         <button> 1 </button>
         <button> 2 </button>
         <button> 3 </button>
-        <button> x </button>
+        <button class=" bg-sky-100  text-gray-600"> x </button>
         <button> . </button>
         <button> 0 </button>
-        <button> = </button>
-        <button> + </button>
+        <button class="bg-slate-50"> = </button>
+        <button class="bg-lime-100  text-gray-600"> + </button>
 
 
        
